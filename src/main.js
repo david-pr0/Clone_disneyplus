c6d08d80277802b7e0e857eabb20e4740e948c8f
David <rafael.santos.jb22@gmail.com>
2024-06-26 16:34:54 -0300

    Criando o faq

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta)
+    }
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (botao) {
@@ -12,6 +17,13 @@ document.addEventListener('DOMContentLoaded', function(){
         })
     }
 })
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+    console.log(elementoPai);
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
